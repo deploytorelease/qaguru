d7d890746d0c16f06d0caf8e8b74d07e38195c62
andrey.ilinykh <andrey.ilinykh@aliexpress.ru>
2022-02-22 13:32:20 +0300

    first test

--- src/test/java/junit/FirstTest.java
+++ src/test/java/junit/FirstTest.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
-public class FisrtTest {
+public class FirstTest {
 
     @BeforeAll
     static void beforeAll() {
